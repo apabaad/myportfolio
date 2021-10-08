b72cd43f2a3c18bbc7d09d0243acacb4c97a9b96
Rupesh Adhikari <apabaad@yahoo.com>
2021-10-08 18:24:45 +1100

    social icons added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Header from './components/Header';
+import SideBar from './components/SideBar';
 
 function App() {
   return (
     <div>
       <Header />
+      <div>
+        <SideBar />
+      </div>
     </div>
   );
 }
---
+++ src/components/SideBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const SideBar = () => {
+  return (
+    <div className="social-icons-div">
+      <i id="fb" class="fab fa-facebook-square social-icons "></i>
+      <i id="linkedin" class="fab fa-linkedin social-icons"></i>
+      <i id="github" class="fab fa-github-square social-icons"></i>
+      <i id="instagram" class="fab fa-instagram-square social-icons"></i>
+      <i id="twitter" class="fab fa-twitter-square social-icons"></i>
+    </div>
+  );
+};
+
+export default SideBar;
